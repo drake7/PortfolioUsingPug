39fd4f20cee4831a30269b8db1d16e479a0acb32
drake7 <Deepak.kumar7v@gmail.com>
2023-02-26 23:32:37 -0700

    added files

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
+
+const mainRoutes = require('./routes');
+const projectRoutes = require('./routes/projects');
+
+app.use(mainRoutes);
+app.use('/projects', projectRoutes);
+
+app.use((req, res, next) => {
+    const err = new Error('Page Not Found');
+    err.status = 404;
+    next(err);
+});
+app.use((err, req, res, next) => {
+    if (!err.status) {
+        err.status = 500;
+        err.message = 'Internal Server Error';
+    }
+    console.log(err);
+    res.render('error', {err});
+});
+app.listen(3000, () => {
+    console.log(`App is listening on port: 3000`);
+});
